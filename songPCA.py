b09e06a1d7a44c19a1994a8264b7414266c9ecdc
jacobnarvades <116325929+jacobnarvades@users.noreply.github.com>
2023-11-23 17:48:14 -0500

    Update songPCA.py

--- songPCA.py
+++ songPCA.py
@@ -3,6 +3,9 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 import math
 from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
+import mplcursors  # Import mplcursors for cursor hover labels
+import matplotlib.cm as cm  # Import colormap for colors
 
 # given a dictionary of songs and their metadata, apply PCA and return similar tracks
 def song_pca(songs_metadata):
@@ -10,12 +13,12 @@ def song_pca(songs_metadata):
     # Convert song metadata to a matrix (rows as songs, columns as features)
     songs_matrix = np.array(list(songs_metadata.values()))
 
-    # Create a mean to compare found songs to
+    # Create a mean to compare found songs, add to matrix/dictionary
     mean_features = np.mean(songs_matrix, axis=0)
     songs_matrix = np.vstack([mean_features, songs_matrix])
 
     d_mean_features = {}
-    d_mean_features['mean_features'] = mean_features
+    d_mean_features['Playlist Mean'] = mean_features
     songs_metadata = {**d_mean_features, **songs_metadata}
 
     # Standardize the data (mean=0, variance=1)
@@ -34,7 +37,31 @@ def song_pca(songs_metadata):
     explained_var_ratio = pca.explained_variance_ratio_
     print(f"Explained variance ratio: {explained_var_ratio}")
 
-    # Choose a reference song index from the PCA-transformed dataset
+    # Calculate colors based on distances from mean and similarity ratios
+    cosine_ratio = 0.7
+    euclidean_ratio = 0.3
+    colors = calculate_colors(songs_pca, songs_pca[0], cosine_ratio, euclidean_ratio)  # Using first point as mean
+
+    # Plot 2D representation of the data with colors
+    plt.figure(figsize=(8, 6))
+    scatter = plt.scatter(songs_pca[:, 0], songs_pca[:, 1], c=colors, alpha=1)
+
+    # Get the song names
+    song_names = list(songs_metadata.keys())
+
+    # Define a function to display labels on hover
+    def on_plot_hover(sel):
+        ind = sel.index
+        sel.annotation.set_text(song_names[ind])
+
+    # Use mplcursors to display annotations on hover
+    mplcursors.cursor(scatter).connect("add", on_plot_hover)
+
+    plt.title('2D Representation of Playlist')
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.show()
+
     reference_song_index = 0  # Change this index to your chosen reference song
 
     # Calculate similarity (Cosine similarity and Euclidean distance) between reference song and all songs
@@ -63,3 +90,22 @@ def song_pca(songs_metadata):
     for song in top_similar_songs:
         print(song, " ", songs_metadata[song])
     print('\n')
+
+# Calculations for the graph display
+def calculate_distances(point, mean):
+    return np.linalg.norm(point - mean)
+
+def calculate_colors(points, mean, cosine_ratio, euclidean_ratio):
+    distances = [calculate_distances(point, mean) for point in points]
+    max_distance = max(distances)
+    similarity_scores = [(cosine_ratio * cosine_similarity([mean], [point])[0][0]) +
+                         (euclidean_ratio * (1 / (1 + np.linalg.norm(mean - point)))) for point in points]
+    max_similarity = max(similarity_scores)
+    scaled_similarities = [similarity / max_similarity for similarity in similarity_scores]
+    tint_values = [1 - similarity for similarity in scaled_similarities]
+    colors = [cm.Blues(1 - tint) for tint in tint_values]
+    return colors
+
+def on_plot_hover(sel):
+    ind = sel.target.index
+    sel.annotation.set_text(song_names[ind])
