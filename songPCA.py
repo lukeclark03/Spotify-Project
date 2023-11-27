0f9b81e06f95b2fdfda5415a4bba5c4d03ebb73f
jacobnarvades <116325929+jacobnarvades@users.noreply.github.com>
2023-11-26 21:55:15 -0500

    Update songPCA.py

--- songPCA.py
+++ songPCA.py
@@ -2,20 +2,26 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 import math
+import os
 from sklearn.metrics.pairwise import cosine_similarity
-import matplotlib.pyplot as plt
 import mplcursors  # Import mplcursors for cursor hover labels
+import matplotlib.pyplot as plt
 import matplotlib.cm as cm  # Import colormap for colors
+import matplotlib.font_manager as fm
 
 # given a dictionary of songs and their metadata, apply PCA and return similar tracks
-def song_pca(songs_metadata, playlist_title='playlist'):
+def song_pca(songs_metadata, song_ids, playlist_title='playlist', mode='default', mean=[], n_components=5):
     # Convert song metadata to a matrix (rows as songs, columns as features)
     songs_matrix = np.array(list(songs_metadata.values()))
 
-    # Create a mean to compare found songs, add to matrix/dictionary
+    # Compute mean and variance to compare found songs, add to matrix/dictionary
     mean_features = np.mean(songs_matrix, axis=0)
     mean_features = [float(f'{val:.2f}') for val in mean_features] 
     songs_matrix = np.vstack([mean_features, songs_matrix])
+
+    #could be useful
+    variance = np.var(np.array(list(songs_metadata.values())), axis=0).tolist()
+    variance = np.sqrt(variance)
 
     d_mean_features = {}
     d_mean_features['reference'] = mean_features
@@ -26,56 +32,77 @@ def song_pca(songs_metadata, playlist_title='playlist'):
     songs_std = scaler.fit_transform(songs_matrix)
 
     # Apply PCA
-    n_components = math.floor(4*math.log(len(songs_metadata), 10)) #maps the n_components to a value based on the amount of tracks dynamically
-    if (n_components > 12):
-        n_components = 12
+    if (mode == 'playlist'):
+        n_components = math.floor(4*math.log(len(songs_metadata), 10)) #maps the n_components to a value based on the amount of tracks dynamically
+        if (n_components > 12):
+            n_components = 12
 
     pca = PCA(n_components)  # Define the number of principal components
     songs_pca = pca.fit_transform(songs_std)
 
     # Explained variance ratio
     explained_var_ratio = pca.explained_variance_ratio_
-    print(f"Explained variance ratio: {explained_var_ratio}")
+    ###################################################
 
     # Calculate colors based on distances from mean and similarity ratios
     cosine_ratio = 0.7
     euclidean_ratio = 0.3
-    colors, similarity_scores = calculate_similarities(songs_pca, songs_pca[0], cosine_ratio, euclidean_ratio)  # Using first point as mean
+    if (mode == 'playlist'):
+        mean = songs_pca[0]
+    colors, similarity_scores = calculate_similarities(songs_pca, mean, cosine_ratio, euclidean_ratio)  # Using first point as mean
 
     # Sort songs by similarity scores
     sorted_indices = sorted(range(len(similarity_scores)), key=lambda i: similarity_scores[i], reverse=True)
 
     # Retrieve top similar songs
-    top_similar_indices = sorted_indices[1:51]  # Exclude the reference song itself
+    top_similar_indices = sorted_indices[1:21]  # Exclude the reference song itself
     top_similar_songs = [list(songs_metadata.keys())[index] for index in top_similar_indices]
+    song_ids = [song_ids[index-1] for index in top_similar_indices]
 
-    reference_song_index = 0  # Change this index to your chosen reference song
-    reference_song_name = list(songs_metadata.keys())[reference_song_index]
-    print(f"\nTop 50 similar songs to: {reference_song_name}")
-    for idx, song_index in enumerate(top_similar_indices):
-        print(top_similar_songs[idx], " ", similarity_scores[song_index])
-    print('\n')
+    # create a pair of songs and ids
+    songs_and_ids = {}
+    length = math.floor(len(songs_metadata)/2)
+    n = 0
+    for song_name, song_id in zip(top_similar_songs, song_ids):
+        if (n == length):
+            break
+        songs_and_ids[song_name] = song_id
+        n += 1
 
-        # Plot 2D representation of the data with colors
+    if (mode == 'recommend'):
+        reference_song_index = 0  # Change this index to your chosen reference song
+        reference_song_name = list(songs_metadata.keys())[reference_song_index]
+        print(f"\n{playlist_title}:")
+        for idx, song_index in enumerate(top_similar_indices):
+            print(f"{top_similar_songs[idx]} {similarity_scores[song_index]:.2f} {song_ids[idx]}")
+        print('\n')
+
+    # Plot 2D representation of the data with colors
     plt.figure(figsize=(8, 6))
     scatter = plt.scatter(songs_pca[:, 0], songs_pca[:, 1], c=colors, alpha=1)
+
+    # fonts
+    font_paths = [
+        'dependencies/NotoColorEmoji.ttf',
+    ]
+    font_props = [fm.FontProperties(fname=path) for path in font_paths]
+    plt.rcParams['font.family'] = ['MS Gothic', 'Arial', 'sans-serif']
+    plt.rcParams['font.sans-serif'] = [prop.get_name() for prop in font_props] + plt.rcParams['font.sans-serif']
 
     # Create labels for annotations
     song_names = list(songs_metadata.keys())
     labels = [f"{song_names[i]}" for i in range(len(song_names))]
 
-    # Get the song names
-    song_names = list(songs_metadata.keys())
-
     # Use mplcursors to display annotations on hover
     cursor = mplcursors.cursor(scatter, hover=True)
     cursor.connect("add", lambda sel: sel.annotation.set_text(f"{labels[sel.index]} {similarity_scores[sel.index]:.2f}"))
 
-    mean_similarity = np.mean(similarity_scores)
     plt.title(f"Similarity plot for: {playlist_title}")
-    plt.xlabel('PC1')
-    plt.ylabel('PC2')
+    plt.xlabel('D1')
+    plt.ylabel('D2')
     plt.show()
+
+    return songs_and_ids, mean, n_components
 
 def calculate_similarities(points, mean, cosine_ratio, euclidean_ratio):
     similarity_scores = [(cosine_ratio * cosine_similarity([mean], [point])[0][0]) +
